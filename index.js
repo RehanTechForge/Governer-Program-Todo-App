9ade9b50c1fe83e4c2b5ead0404c5ab509b33c9d
Muhammad Rehan <muhammadrehan125768@gmail.com>
2024-05-12 09:06:29 +0500

    create a todo project

---
+++ index.js
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+let todos = [];
+let condition = true;
+while (condition) {
+    const todoQuestion = await inquirer.prompt([
+        {
+            type: "input",
+            name: "todo",
+            message: "What needs to be done?"
+        },
+        {
+            type: "confirm",
+            name: "nextQuestion",
+            message: "Do you want to continue the next question",
+            default: true,
+        }
+    ]);
+    todos.push(todoQuestion.todo);
+    console.log(chalk.green("Added: ") + chalk.bold.yellow(todoQuestion.todo)); // Adding color
+    console.log(chalk.blue("Current Todos:")); // Adding color for the list
+    todos.forEach((todo, index) => {
+        console.log(chalk.blue(`${index + 1}. ${todo}`)); // Adding color for the list items
+    });
+    condition = todoQuestion.nextQuestion;
+}
